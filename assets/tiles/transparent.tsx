d3ccb282029c11f5b3b530bc51d218077982e08c
Arthur Cosentino <arthurcose@gmail.com>
2021-08-19 18:36:06 -0400

    Add transparent tile

---
+++ assets/tiles/transparent.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="transparent" tilewidth="8" tileheight="8" tilecount="1" columns="1">
+ <tileoffset x="32" y="-16"/>
+ <image source="transparent.png" width="8" height="8"/>
+</tileset>
