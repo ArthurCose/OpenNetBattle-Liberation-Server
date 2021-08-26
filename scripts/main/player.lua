756a8047d5681984143115703e3fe1020a02a27f
Arthur Cosentino <arthurcose@gmail.com>
2021-08-25 20:02:26 -0400

    Resolve player promises created after disconnecting

--- scripts/main/player.lua
+++ scripts/main/player.lua
@@ -1,8 +1,14 @@
 -- private functions
 
-local function create_textbox_promise(textbox_promise_resolvers)
+local function create_textbox_promise(self)
+  if self.disconnected then
+    return Async.create_promise(function(resolve)
+      resolve()
+    end)
+  end
+
   return Async.create_promise(function(resolve)
-    textbox_promise_resolvers[#textbox_promise_resolvers+1] = resolve
+    self.textbox_promise_resolvers[#self.textbox_promise_resolvers+1] = resolve
   end)
 end
 
@@ -22,6 +28,7 @@ function Player:new(player_id)
     x = position.x,
     y = position.y,
     z = position.z,
+    disconnected = false
   }
 
   setmetatable(player, self)
@@ -34,7 +41,7 @@ end
 function Player:message(message, texture_path, animation_path)
   Net.message_player(self.id, message, texture_path, animation_path)
 
-  return create_textbox_promise(self.textbox_promise_resolvers)
+  return create_textbox_promise(self)
 end
 
 -- all messages to this player should be made through the session while the session is alive
@@ -46,7 +53,7 @@ end
 function Player:question(question, texture_path, animation_path)
   Net.question_player(self.id, question, texture_path, animation_path)
 
-  return create_textbox_promise(self.textbox_promise_resolvers)
+  return create_textbox_promise(self)
 end
 
 -- all questions to this player should be made through the session while the session is alive
@@ -58,7 +65,7 @@ end
 function Player:quiz(a, b, c, texture_path, animation_path)
   Net.quiz_player(self.id, a, b, c, texture_path, animation_path)
 
-  return create_textbox_promise(self.textbox_promise_resolvers)
+  return create_textbox_promise(self)
 end
 
 function Player:is_battling()
@@ -67,6 +74,12 @@ end
 
 -- all quizzes to this player should be made through the session while the session is alive
 function Player:initiate_encounter(asset_path)
+  if self.disconnected then
+    return Async.create_promise(function(resolve)
+      resolve({ran = true})
+    end)
+  end
+
   if self:is_battling() then
     error("This player is already in a battle")
   end
@@ -92,6 +105,8 @@ function Player:handle_battle_results(stats)
 end
 
 function Player:handle_disconnect()
+  self.disconnected = true
+
   for _, resolve in ipairs(self.textbox_promise_resolvers) do
     resolve()
   end
