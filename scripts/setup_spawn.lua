cff729ae474bb359174cefb3846251cec60301be
Arthur Cosentino <arthurcose@gmail.com>
2022-01-02 21:06:29 -0500

    Revert spawn logic

--- scripts/setup_spawn.lua
+++ scripts/setup_spawn.lua
@@ -15,20 +15,9 @@ for i, area_id in ipairs(areas) do
   end
 end
 
-local spawn_data = {}
-
 function handle_player_request(player_id, data)
-  spawn_data[player_id] = data
-end
-
-function handle_player_connect(player_id)
-  if spawn_data[player_id] == "iceberg" then
+  if data == "iceberg" then
     local spawn = Net.get_object_by_name("default", "Iceberg Spawn")
     Net.teleport_player(player_id, true, spawn.x, spawn.y, spawn.z, Direction.DOWN_RIGHT)
   end
-  spawn_data[player_id] = nil
-end
-
-function handle_player_disconnect(player_id)
-  spawn_data[player_id] = nil
 end
